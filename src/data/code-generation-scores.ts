8cd6157aca72f9548ab2f9e6c891d205246c9918
GratuitPourLesDevs <julien.taverne+gratuitpourlesdevs@gmail.com>
2026-08-18 22:44:16 +0200

    Ajoute les scores des outils de génération de code

---
+++ src/data/code-generation-scores.ts
@@ -0,0 +1,8 @@
+export const CODE_GENERATION_QUOTA_SCORES = {
+  appinvento: 15,
+  dhiwise: 10,
+  'karbon-sites': 5,
+  metalama: 25,
+  supermaven: 15,
+  v0: 15,
+} satisfies Record<string, 5 | 10 | 15 | 20 | 25>;
